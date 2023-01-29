85815605c414d179f2b14379137d5bd537097369
ilive20221 <123864240+ilive20221@users.noreply.github.com>
2023-01-30 02:45:30 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,15 @@ var express = require('express');
 const fs = require('fs')
 var https = require('http');
 var dataList = JSON.parse(fs.readFileSync('./index.json', 'utf8'));
-//var data = [];
-//https.get(url, function (res) {
-//  res.on('data', function(chunk) {
-//    data.push(chunk);
-//  }).on('end', function() {
-//    var events   = Buffer.concat(data);
-//    // オブジェクト形式に変換する(パースする)
-//    var r = JSON.parse(events);
-//    //console.log(r);
-//  });
-//});
-
 
 var router = express.Router();
-//const da = fs.readFileSync(`index.json`, "utf-8");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title:"アイライブ",
+    title:"【有料会員限定】アイライブ鍵",
     content:dataList
   });
 });
 
 module.exports = router;
+
