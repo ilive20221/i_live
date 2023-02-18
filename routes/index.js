5e48bc0aef213d12ff38d345c047290f12f10d27
ilive20221 <123864240+ilive20221@users.noreply.github.com>
2023-02-18 23:17:54 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title:"【投稿一覧】アイライブ",
+    title:"アイライブ",
     content:dataList
   });
 });
