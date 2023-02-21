f63f3ac829343815b3583bf4128b96df88881f3a
ilive20221 <123864240+ilive20221@users.noreply.github.com>
2023-02-22 01:02:38 +0900

    Add files via upload

---
+++ routes/pan.js
@@ -0,0 +1,17 @@
+var express = require('express');
+const fs = require('fs')
+var https = require('http');
+var dataList = JSON.parse(fs.readFileSync('./index.json', 'utf8'));
+const datalen = Object.keys(dataList).length;
+var router = express.Router();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('pan', {
+    title:"アイライブ",
+    id:'＠t53942800',
+    content:dataList,
+    len:datalen
+  });
+});
+
+module.exports = router;
